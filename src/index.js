d1629264a1114f78e5431b6f34814ebf45ea36c2
Silvia Zandoli <silvia.zandoli2@studio.unibo.it>
2021-08-10 10:12:33 +0200

    tolto body parser

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const db = require("./config/config").get(process.env.NODE_ENV);
 
