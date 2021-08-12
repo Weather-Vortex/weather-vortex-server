2d101ac54e1d722569b36e9fdd324674304e8202
Silvia Zandoli <silvia.zandoli2@studio.unibo.it>
2021-08-12 10:25:57 +0200

    comments

--- src/storages/user.storage.js
+++ src/storages/user.storage.js
@@ -1,11 +1,12 @@
 const User = require("../models/user.model");
 const db = require("../config/config").get(process.env.NODE_ENV);
-//administrator operations
+
 /**
  * Create a new user and send back data.
  * @param {express.Request} req Express request from route.
  * @param {express.Response} res Express response from route.
  */
+//administrator operations
 const createUser = (req, res) => {
   const user = req.body;
   const newUser = new userModel(user);
@@ -37,12 +38,12 @@ const getUser = (req, res) => {
 
 //these options are for the users-> to do in auth storage
 const updateUser = (req, res) => {
-  // TODO: Update some info about user.
+  // TODO: An administrator can update info of a user?
   console.log("Update user", req.body);
 };
 
 const deleteUser = (req, res) => {
-  // TODO: We can delete users?
+  // TODO: An administrator can delete users?
   console.log("Delete user", req.params.id);
 };
 
