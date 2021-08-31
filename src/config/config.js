442bec11f752c62d3113430f42270df983e4615b
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-08-31 14:35:23 +0200

    Remove reference to DATABASE var.

--- src/config/config.js
+++ src/config/config.js
@@ -1,22 +1,19 @@
 //npm install express body-parser cookie-parser bcrypt mongoose jsonwebtoken nodemon
-const config={
-    production :{
-        SECRET: process.env.SECRET,
-        DATABASE: process.env.MONGODB_URI,
-        USEREMAIL:process.env.USER_EMAIL,
-        PWD:process.env.PWD,
-        CONFIRM:process.env.CONFIRM
-    },
-    default : {
-        SECRET: 'mysecretkey',
-        DATABASE: 'mongodb://localhost:27017/test',
-        USEREMAIL:'weathervortex2@gmail.com',
-        PWD:'progettoWeb',
-        CONFIRM:'http://localhost:12000/api/confirm/'
-    }
-}
+const config = {
+  production: {
+    SECRET: process.env.SECRET,
+    USEREMAIL: process.env.USER_EMAIL,
+    PWD: process.env.PWD,
+    CONFIRM: process.env.CONFIRM,
+  },
+  default: {
+    SECRET: "mysecretkey",
+    USEREMAIL: "weathervortex2@gmail.com",
+    PWD: "progettoWeb",
+    CONFIRM: "http://localhost:12000/api/confirm/",
+  },
+};
 
-
-exports.get = function get(env){
-    return config[env] || config.default
-}
+exports.get = function get(env) {
+  return config[env] || config.default;
+};
