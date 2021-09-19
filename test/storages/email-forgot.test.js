9710dde5b0835530536d125c7182c4596d659b34
Silvia Zandoli <silvia.zandoli2@studio.unibo.it>
2021-09-19 12:34:07 +0200

    primi test sulla email, todo forgot pwd

--- test/storages/email-forgot.test.js
+++ test/storages/email-forgot.test.js
@@ -32,3 +32,51 @@ describe("Mocha + Mockery Test", function () {
 
   //https://github.com/doublesharp/nodemailer-mock/blob/master/test/nodemailer-mock-tests.js
 });*/
+//https://www.npmjs.com/package/nodemailer-mock-transport
+var nodemailer = require("nodemailer");
+const mockery = require("mockery");
+var mockTransport = require("../index-mock.js");
+
+describe("mock-transport", function () {
+  it("should store configuration options so that they can be asserted against", function () {
+    var transport = mockTransport({
+      foo: "bar",
+    });
+    transport.options.foo.should.equal("bar");
+  });
+
+  it("should store emails sent with nodemailer, so that they can be asserted against", function () {
+    var transport = mockTransport({
+      foo: "bar",
+    });
+
+    var transporter = nodemailer.createTransport(transport);
+
+    transporter.sendMail({
+      from: "silviadolomiti@gmail.com",
+      to: "silviadolomiti@gmail.com",
+      subject: "hello",
+      text: "hello world!",
+    });
+
+    transport.sentMail.length.should.equal(1);
+    transport.sentMail[0].data.to.should.equal("silviadolomiti@gmail.com");
+    transport.sentMail[0].message.content.should.equal("hello world!");
+  });
+
+  it("should return an error and not send an email if there is no `to` in the mail data object", function () {
+    var transport = mockTransport({
+      foo: "bar",
+    });
+
+    var transporter = nodemailer.createTransport(transport);
+
+    transporter.sendMail({
+      from: "silviadolomiti@gmail.com",
+      subject: "hello",
+      text: "hello world!",
+    });
+
+    transport.sentMail.length.should.equal(0);
+  });
+});
