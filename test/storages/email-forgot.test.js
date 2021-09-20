84956f2aea3596529a9bcf9215e0b5a12f1a7830
Silvia Zandoli <silvia.zandoli2@studio.unibo.it>
2021-09-20 13:06:57 +0200

    tolto commento

--- test/storages/email-forgot.test.js
+++ test/storages/email-forgot.test.js
@@ -42,11 +42,6 @@ describe("forgotPassword", () => {
           });
       });
     });
-    //nel text metto il token, poi lo andrò a prendere da lì per fare la reset password
-    /*
-    transport.sentMail.length.should.equal(1);
-    transport.sentMail[0].data.to.should.equal("silviadolomiti@gmail.com");
-    transport.sentMail[0].message.content.should.equal("hello world!");*/
   });
 });
 
