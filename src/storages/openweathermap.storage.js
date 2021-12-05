be86409f7a89a9386693094c51266b82c6e774cc
Daniele Tentoni <d.tentoni@wedoit.io>
2021-12-05 21:34:08 +0100

    Remove deprecated methods.

--- src/storages/openweathermap.storage.js
+++ src/storages/openweathermap.storage.js
@@ -103,29 +103,6 @@ const mapFields = (forecast) => {
 };
 
 /**
- * Retrieve weather forecasts for given city.
- * @deprecated Since 0.4.0, this method will be deprecated for moreDayByCity and will be removed in a next release.
- * @param {String} city_name City Name for weather forecasts.
- * @returns {Promise<any>} Weather Forecast Promise.
- */
-const fourDayForecastByCityRequest = (city_name) => {
-  const resource = `forecast?q=${city_name}`;
-  return provider.makeRequest(resource);
-};
-
-/**
- * Return request promise for forecast for given position.
- * @deprecated Since 0.4.0, this method will be deprecated for moreDayByLocation and will be removed in a next release.
- * @param {Number} latitude Latitude of the position.
- * @param {Number} longitude Longitude of the position.
- * @returns {Promise<any>} Weather Forecast Promise.
- */
-const fourDayForecastByLocationRequest = (latitude, longitude) => {
-  const resource = `forecast?lat=${latitude}&lon=${longitude}`;
-  return provider.makeRequest(resource);
-};
-
-/**
  * Return request promise for forecast for given position.
  * @param {Number} latitude Latitude of the position.
  * @param {Number} longitude Longitude of the position.
